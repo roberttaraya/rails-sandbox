3179621af1119a8441464b364462df9eafbb86f2
Robert Taraya <robert.taraya@gmail.com>
2014-11-07 14:09:54 -0600

    create MVC for users, add user objects to db

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  resources :users
+  root to: 'users#index', via: :get
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107200052) do
+ActiveRecord::Schema.define(version: 20141107200450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,13 @@ ActiveRecord::Schema.define(version: 20141107200052) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
