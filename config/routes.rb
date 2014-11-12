1776fa90b6630b591a1200481204fce06753e86b
Robert Taraya <robert.taraya@gmail.com>
2014-11-11 22:43:08 -0600

    move comfy route to bottom of file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   comfy_route :cms_admin, :path => '/admin'
 
-  # Make sure this routeset is defined last
-  comfy_route :cms, :path => '/', :sitemap => false
 
   resources :users
   root to: 'users#index', via: :get
+
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/', :sitemap => false
 end
